f25f97d43270122480b617c3563a9b3b785c14b6
Anders Janmyr <anders@janmyr.com>
2024-08-07 09:53:24 +0200

    More config

--- lua/options.lua
+++ lua/options.lua
@@ -1,65 +1,69 @@
 -- [[ Setting options ]]
 -- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
+}
+vim.opt.timeoutlen = 500 -- the delay before the leader kicks in
 
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+vim.opt.background="dark"
+vim.opt.backup = true -- make backup files
+vim.opt.backupdir= os.getenv("HOME") .. "/.vim/backup" -- where to put backup files
+vim.opt.directory= os.getenv("HOME") .. "/.vim/swap" -- where to put swap files
+vim.opt.cmdheight=2 -- Make room for larger commands without Enter
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
 
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.opt.cursorline = true
+vim.opt.enc="utf-8"
+vim.opt.expandtab = true
+vim.opt.foldenable = true
+vim.opt.foldlevelstart=20
+vim.opt.foldmethod="syntax"
+vim.opt.formatoptions="qrn1"
+vim.opt.gdefault = true
+vim.opt.grepprg="ack"
+vim.opt.hidden = true -- you can change buffers without saving
+vim.opt.hlsearch = true --Highlight search"
+vim.opt.history=400 --Larger history
+vim.opt.ignorecase = true -- case insensitive by default
+vim.opt.lbr =true
+vim.opt.list =true
+vim.opt.compatible = false -- explicitly get out of vi-compatible mode
+vim.opt.exrc = false -- don't use local version of .(g)vimrc, .exrc
+vim.opt.number = true -- enable line numbers
+vim.opt.colorcolumn= "85"
+vim.opt.undofile = true
+vim.opt.undodir=os.getenv("HOME") .. "/.vim/undo" -- where to put backup files
+vim.opt.shell="bash"
+vim.opt.splitbelow=true
+vim.opt.splitright=true
+vim.opt.textwidth=79
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.smarttab=true
+vim.opt.shiftwidth=4
+vim.opt.shortmess="atI" -- Use short messages to prevent press enter...
+vim.opt.smartcase=true -- Smart searching for capitalized words
+vim.opt.spell=true
+vim.opt.statusline="%<%f %h%m%r%{fugitive#statusline()}%=%-14.(%l,%c%V%) %P"
+vim.opt.visualbell=true
+vim.opt.wildmode="list:longest,full" --Magic completion menu--
+vim.opt.wrap=true
+vim.opt.autowrite=true
+vim.g.solarized_termcolors=256
+vim.g.neoformat_try_node_exe = 1
 
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.g.python3_host_prog = '/Users/andersjanmyr/.pyenv/versions/neovim3/bin/python'
+
+vim.opt.scrolloff = 5
+vim.opt.breakindent = true
+vim.opt.updatetime = 250
+vim.opt.inccommand = 'split'
 
 -- vim: ts=2 sts=2 sw=2 et
