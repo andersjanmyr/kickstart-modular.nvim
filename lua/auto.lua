8ac301a1dbc0551f6ad2274cc33405b79f5af099
Anders Janmyr <anders@janmyr.com>
2024-11-07 14:28:33 +0100

    Reformat

--- lua/auto.lua
+++ lua/auto.lua
@@ -1,73 +1,38 @@
-vim.api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    {
-        pattern = { "*.txt", "*.md", "*.tex" },
-        command = "setlocal spell"
-    }
-)
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.txt', '*.md', '*.tex' },
+  command = 'setlocal spell',
+})
 
-vim.api.nvim_create_autocmd(
-    "FileType",
-    {
-        pattern = { "go", "lua" },
-        command = "setlocal nolist noexpandtab"
-    }
-)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'go', 'lua' },
+  command = 'setlocal nolist noexpandtab',
+})
 
-vim.api.nvim_create_autocmd(
-    "FileType",
-    {
-        pattern = { 'css', 'html', 'javascript', 'json','sh',  'typescript' },
-        command = "setlocal expandtab ts=4 sts=4 sw=4"
-    }
-)
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'css', 'html', 'javascript', 'json', 'sh', 'typescript' },
+  command = 'setlocal expandtab ts=2 sts=2 sw=2',
+})
 
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern = { '*.js', '*.json','*.ts' },
-        command = "Neoformat"
-    }
-)
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*.js', '*.json', '*.ts' },
+  command = 'Neoformat',
+})
 
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*.tf' },
+  command = 'TerraformFmt',
+})
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.txt', '*.md', '*.tex' },
+  command = 'setlocal spell',
+})
 
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern ={ '*.tf' },
-        command = "TerraformFmt"
-    }
-)
-vim.api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    {
-        pattern = { "*.txt", "*.md", "*.tex" },
-        command = "setlocal spell"
-    }
-)
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*.js', '*.json', '*.ts' },
+  command = 'Neoformat',
+})
 
-vim.api.nvim_create_autocmd(
-    "FileType",
-    {
-        pattern = { 'css', 'html', 'javascript', 'json','sh',  'typescript' },
-        command = "setlocal expandtab ts=4 sts=4 sw=4"
-    }
-)
-
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern = { '*.js', '*.json','*.ts' },
-        command = "Neoformat"
-    }
-)
-
-
-vim.api.nvim_create_autocmd(
-    "BufWritePost",
-    {
-        pattern ={ '*.tf' },
-        command = "TerraformFmt"
-    }
-)
-
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = { '*.tf' },
+  command = 'TerraformFmt',
+})
