c27aa98b3808cb437b2adb3481814521ffa61f7c
Anders Janmyr <anders@janmyr.com>
2024-11-11 15:40:06 +0100

    Treesitter methods

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -80,6 +80,7 @@ return {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>f', builtin.git_files, {})
       vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>m', builtin.treesitter, {})
       vim.keymap.set('n', '<leader>h', builtin.command_history, {})
     end,
   },
