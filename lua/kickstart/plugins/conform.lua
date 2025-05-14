1777b8d78712f93301b62593ed0ad1ae9ca9bf48
Anders Janmyr <anders@janmyr.com>
2025-05-14 13:38:07 +0200

    Remove vim-css-color due to issues

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
       {
         '==',
         function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
+          require('conform').format({ async = true, lsp_format = 'fallback' })
         end,
         mode = '',
         desc = '[F]ormat buffer',
@@ -19,7 +19,11 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {
+          c = true,
+          cpp = true,
+          erlang = true,
+        }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
